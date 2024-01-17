d561f3605813e57182426e75ca106bd2694536c6
skuzzis <andreieurt@gmail.com>
2024-01-17 20:37:26 +0200

    update(cross-compiler): setup.sh

--- cross-compiler/setup.sh
+++ cross-compiler/setup.sh
@@ -2,18 +2,18 @@
 
 FOLDER=${1}
 
-cd /${FOLDER}
+cd /$FOLDER
 
-rm -rf /${FOLDER}/build
+rm -rf /$FOLDER/build
 
-if [ -f /${FOLDER}/setup.sh ]; then
-    bash /${FOLDER}/setup.sh
+if [ -f /$FOLDER/setup.sh ]; then
+    bash /$FOLDER/setup.sh
 else
-    mkdir /${FOLDER}/build
-    cd /${FOLDER}/build
-    CC=gcc CXX=g++ python3 ../configure.py --plugin-name=${FOLDER} -s cs2
-    cd /${FOLDER}
+    mkdir /$FOLDER/build
+    cd /$FOLDER/build
+    CC=gcc CXX=g++ python3 ../configure.py --plugin-name=$FOLDER -s cs2
+    cd /$FOLDER
 fi
 
-cd /${FOLDER}/build
+cd /$FOLDER/build
 ambuild
