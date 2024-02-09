f961b97fa8272c9df264075c311419d7a005cf42
skuzzis <andreieurt@gmail.com>
2024-02-09 19:55:34 +0200

    fix(plugin-cross-compiler): Git clone

--- plugin-cross-compiler/setup.sh
+++ plugin-cross-compiler/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd tempfolder
-git clone github.com/swiftly-solution/swiftly
+git clone https://github.com/swiftly-solution/swiftly
 cd swiftly; mv plugin_files/scripting/* ..; cd ..
 
 cd /tempfolder/${1}
